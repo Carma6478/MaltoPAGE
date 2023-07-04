68bec46d4c03ae3a11a371d5a997ae025b2d5ebb
Luke Capuras <135145871+Carma6478@users.noreply.github.com>
2023-07-04 14:25:17 +0800

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,3 +1,4 @@
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz45OkEn-dxUGN5r7hWIv-571Cq9lj9HLzzzZfvis4_E8F7vJC8icVyJrrZDaS1GQie/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
